5f15c5138fb8290648502d24354f576e14ccc543
Ian Miller <irmiller22@gmail.com>
2014-11-14 11:23:08 -0500

    generated post migration, model

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ActiveRecord::Base
+  has_many :taggables
+  has_many :tags, through: :taggables
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
+end
