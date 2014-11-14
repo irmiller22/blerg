83d5463ca504dab64d3dc9830c731b1ccb425ca5
Ian Miller <irmiller22@gmail.com>
2014-11-14 14:07:44 -0500

    added count method to tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
   has_many :taggables
   has_many :posts, through: :taggables
+
+  def self.counts
+    self.select("name, count(taggables.tag_id) as count").joins(:taggables).group("taggables.tag_id")
+  end
 end
