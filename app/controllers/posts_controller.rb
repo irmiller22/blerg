307e7f1b67893466542761e5ed8e35da45c62f40
Ian Miller <irmiller22@gmail.com>
2014-11-14 13:47:27 -0500

    updated index and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.js
+      else
+        format.html { render root_path }
+      end
+    end
   end
 
   private
